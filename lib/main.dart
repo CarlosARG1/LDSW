d6697583d7fba293658f82275663aaad7141bcfc
Carlos Alberto Rodriguez Garcia <76966023+CarlosARG1@users.noreply.github.com>
2024-11-01 18:28:21 -0600

    3.6. Peticiones HTTP
    La pantalla de inicio será llamada desde su respectivo directorio.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'home_screen.dart';
 
 void main() {
   runApp(MoviesCatApp());
@@ -11,73 +10,8 @@ class MoviesCatApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MoviesCat',
-      theme: ThemeData.dark(), 
-      home: Scaffold(
-        body: Stack(
-          fit: StackFit.expand,
-          children: <Widget>[
-            Image.asset(
-              'background.jpg', 
-              fit: BoxFit.cover,
-            ),
-            Container(
-              color: Colors.black.withOpacity(0.6), 
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Icon(
-                  Icons.movie,
-                  color: Colors.white,
-                  size: 80,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'MoviesCat',
-                    style: TextStyle(
-                      fontSize: 48,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                Text(
-                  'Bienvenido',
-                  style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.white,
-                  ),
-                ),
-                SizedBox(height: 20),
-                ElevatedButton.icon(
-                  onPressed: () {
-                  },
-                  icon: Icon(Icons.explore),
-                  label: Text('Explorar Películas'),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 75, 12, 12),
-                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-                    textStyle: TextStyle(fontSize: 18),
-                  ),
-                ),
-                Spacer(),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Text(
-                    'Sobre Nosotros',
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.white,
-                      decoration: TextDecoration.underline,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
+      theme: ThemeData.dark(),
+      home: HomeScreen(),
     );
   }
 }
